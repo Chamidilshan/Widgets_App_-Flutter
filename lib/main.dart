83e6d835061fb32356e53da77fad1638336cc2c5
Your Name <chamidilshan55@gmail.com>
2023-01-19 19:19:18 +0530

    added buttons for appbar

--- lib/main.dart
+++ lib/main.dart
@@ -38,138 +38,155 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.purpleAccent,
+        title: Text('Widgets App'),
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications_active),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Open Bottom Sheet'),
-              onPressed: () {
-                showModalBottomSheet(
-                    context: context,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(40.0),
-                        bottom: Radius.circular(40.0),
-                      )
-                    ),
-                    builder: (context) => Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('This is bottom sheet'),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                  Center(
-                    child: ElevatedButton(
-                      child: Text('Close'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        ExpansionTile(
-                          title: Text(
-                            'Android',
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text('Open Bottom Sheet'),
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(40.0),
+                          bottom: Radius.circular(40.0),
+                        )
+                      ),
+                      builder: (context) => Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('This is bottom sheet'),
+                          SizedBox(
+                            height: 20.0,
                           ),
-                          children: [
-                            ListTile(title: Text('Java', style: TextStyle(fontSize: 10.0),),
-                              onTap: () {},
-                            ),
-                            ListTile(title: Text('Kotlin', style: TextStyle(fontSize: 10.0),),
-                              onTap: () {},
-                            ),
-                            ListTile(title: Text('Flutter', style: TextStyle(fontSize: 10.0),),
-                              onTap: () {},
-                            ),
-                            ListTile(title: Text('Kotlin', style: TextStyle(fontSize: 10.0),),
-                              onTap: () {},
-                            ),
-                          ],
-                          onExpansionChanged: (isExpanded){
-                            print('Expanded: $isExpanded');
-                          },
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        CheckboxListTile(
-                          title: Text(
-                            'This app has more features'
-                          ),
-                          controlAffinity: ListTileControlAffinity.leading,
-                          value: isChecked, onChanged: (value){
-                          setState(() {
-                            isChecked = value!;
-                          });
+                    Center(
+                      child: ElevatedButton(
+                        child: Text('Close'),
+                        onPressed: () {
+                          Navigator.pop(context);
                         },
-                        activeColor: Colors.red,
-                          checkColor: Colors.white,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                  builder: (context) => AlertDialog(
-                                    title: Text(
-                                      'Welcome To Flutter'
-                                    ),
-                                    content: Text(
-                                      'Flutter is an open source UI toolkit for building beautiful, natively compiled applications for mobile, web, desktop, and embedded devices from a single codebase. Flutter is primarily funded by Google with contributors from all around the world.'
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: Text(
-                                            'Cancel'
-                                        ),
+                      ),
+                    ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          ExpansionTile(
+                            title: Text(
+                              'Android',
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            children: [
+                              ListTile(title: Text('Java', style: TextStyle(fontSize: 10.0),),
+                                onTap: () {},
+                              ),
+                              ListTile(title: Text('Kotlin', style: TextStyle(fontSize: 10.0),),
+                                onTap: () {},
+                              ),
+                              ListTile(title: Text('Flutter', style: TextStyle(fontSize: 10.0),),
+                                onTap: () {},
+                              ),
+                              ListTile(title: Text('Kotlin', style: TextStyle(fontSize: 10.0),),
+                                onTap: () {},
+                              ),
+                            ],
+                            onExpansionChanged: (isExpanded){
+                              print('Expanded: $isExpanded');
+                            },
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          CheckboxListTile(
+                            title: Text(
+                              'This app has more features'
+                            ),
+                            controlAffinity: ListTileControlAffinity.leading,
+                            value: isChecked, onChanged: (value){
+                            setState(() {
+                              isChecked = value!;
+                            });
+                          },
+                          activeColor: Colors.red,
+                            checkColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: Text(
+                                        'Welcome To Flutter'
                                       ),
-                                      TextButton(
+                                      content: Text(
+                                        'Flutter is an open source UI toolkit for building beautiful, natively compiled applications for mobile, web, desktop, and embedded devices from a single codebase. Flutter is primarily funded by Google with contributors from all around the world.'
+                                      ),
+                                      actions: [
+                                        TextButton(
                                           onPressed: () {
                                             Navigator.pop(context);
                                           },
                                           child: Text(
-                                              'OK'
+                                              'Cancel'
                                           ),
-                                      ),
-                                    ],
-                                  ),
-                              );
-                            },
-                            child: Text(
-                              'Open Dialog'
-                            ),
-                        ),
-                        DropdownButton<String>(
-                          value: selectedItem,
-                            items: items.map((item) => DropdownMenuItem<String>(
-                                value: item,
-                                child: Text(item),),
-                            ).toList(),
-                            onChanged: (item) => {
-                            setState(() => selectedItem = item)
-                            }
-                        )
-                      ],
-                    ),
-                );
-              },
-            ),
-          ],
+                                        ),
+                                        TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text(
+                                                'OK'
+                                            ),
+                                        ),
+                                      ],
+                                    ),
+                                );
+                              },
+                              child: Text(
+                                'Open Dialog'
+                              ),
+                          ),
+                          DropdownButton<String>(
+                            value: selectedItem,
+                              items: items.map((item) => DropdownMenuItem<String>(
+                                  value: item,
+                                  child: Text(item),),
+                              ).toList(),
+                              onChanged: (item) => {
+                              setState(() => selectedItem = item)
+                              }
+                          )
+                        ],
+                      ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
